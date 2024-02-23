ac2978836d47d4907181d9a49d1a5bd28ce8e506
mujahidking864 <160350978+mujahidking864@users.noreply.github.com>
2024-02-23 10:50:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.antitags = true;
 global.autoTyping = false;                //auto tying by default off.
 global.autoRecord = true;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
@@ -21,13 +21,13 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["+994402227858"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+994402227858"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+994402227858"];
+global.OwnerName = "𝗠𝗨𝗝𝗔𝗛𝗜𝗗 𝗛𝗔𝗖𝗞𝗘𝗥";
+global.BotName = "𝗗𝗔𝗥𝗞 𝗠𝗨𝗝𝗔𝗛𝗜𝗗";
+global.packname = "☠️ 𝗠𝗨𝗝𝗔𝗛𝗜𝗗 ☠️";                             //Do not change.
+global.author = "𝗠𝗨𝗝𝗔𝗛𝗜𝗗";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
@@ -42,8 +42,8 @@ global.openAiAPI = "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";
 
 //
 global.location = "mathara, Sri Lankan";
-global.reactmoji = "💞";
-global.themeemoji = "😂";
+global.reactmoji = "💔";
+global.themeemoji = "☠️";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
 global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
 global.lolhuman = "kushan";
